5154d30112adab2580f67cab18835daf579fef43
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-09-17 17:52:20 +0300

    auth-active

--- firebase/config.js
+++ firebase/config.js
@@ -6,12 +6,13 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
+  apiKey: "AIzaSyDX0u-Z8MrwZ7x6hJqcJoX2O0TwpBp-WCk",
+  authDomain: "reactnativehw-399015.firebaseapp.com",
+  databaseURL: "https://reactnativehw-399015-default-rtdb.firebaseio.com",
+  projectId: "reactnativehw-399015",
+  storageBucket: "reactnativehw-399015.appspot.com",
+  messagingSenderId: "815860457079",
+  appId: "1:815860457079:android:021539f01bfc72d3bdb072",
   measurementId: "G-measurement-id",
 };
 
